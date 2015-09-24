7b5268a5db56cdcb46ccd5e28d595348e8374cb2
hustlzp <hustlzp@qq.com>
2015-09-24 11:07:03 +0800

    Initial Commit

---
+++ spark/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  spark
+//
+//  Created by hustlzp on 15/9/24.
+//  Copyright © 2015年 hustlzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
